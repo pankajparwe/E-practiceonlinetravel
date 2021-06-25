2a20b906673407b6ba1e1ba3e27e0c9497f5c1ea
Pankaj_parwe <pankaj.parwe@gmail.com>
2021-06-25 21:46:31 +0530

    dffs

---
+++ src/test/java/udemydropdown/e2e.java
@@ -0,0 +1,44 @@
+package udemydropdown;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class e2e {
+	public static void main(String[] args) {
+	
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Desktop\\chromedriver\\chromedriver.exe");
+	WebDriver driver =new ChromeDriver();
+	driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+	//calender selection
+			driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+			driver.findElement(By.xpath("//a[@value='BLR']")).click();
+			
+			driver.findElement(By.xpath("(//a[@value='MAA']) [2]")).click();
+			//(//a[@value='MAA'])[2]
+	//calender
+	     //   driver.findElement(By.xpath("//a[@class='ui-state-default ui-state-highlight']")).click();
+	driver.findElement(By.cssSelector("#ui-datepicker-div > div.ui-datepicker-group.ui-datepicker-group-first > table > tbody > tr:nth-child(4) > td.ui-datepicker-days-cell-over.ui-datepicker-today > a")).click();
+			// passenger value
+	        
+	        driver.findElement(By.id("divpaxinfo")).click();
+		    for(int i=1; i<5; i++)
+		    {
+		    	driver.findElement(By.id("hrefIncAdt")).click();
+
+		    }
+		    Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult" );
+		    System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		    
+//senior citizen checkbox
+		    driver.findElement(By.id("ctl00_mainContent_chk_SeniorCitizenDiscount")).click();
+		    
+		    
+		    
+
+
+
+}
+	
+}
