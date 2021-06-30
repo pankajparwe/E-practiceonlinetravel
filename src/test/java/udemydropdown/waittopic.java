6d99c2d8172e49cca3139ddfb5084c59846d2278
Pankaj_parwe <pankaj.parwe@gmail.com>
2021-06-30 20:18:30 +0530

    ff

--- src/test/java/udemydropdown/waittopic.java
+++ src/test/java/udemydropdown/waittopic.java
@@ -1,28 +1,42 @@
-package GreenCart;
+package udemydropdown;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class FindingThroughGeneric {
+public class waittopic {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		WebDriver driver;
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Desktop\\chromedriver\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		String[] itemneeded = {"Brocolli" ,"Beetroot", "Cucumber"};
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+		String[] itemneeded = {"Brocolli" ,"Beetroot", "Cucumber"};
+		additems(driver, itemneeded);
+		driver.findElement(By.xpath("//img[@alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");		
+	    driver.findElement(By.cssSelector("button.promoBtn")).click();
+	    
+	    WebDriverWait w =new WebDriverWait(driver, 5);
+	    w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+	   System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText()); 
+
+	}
+	
+	public static void additems(WebDriver driver, String[] itemneeded ) {
 		List<WebElement> Products= driver.findElements(By.xpath("//h4[@class='product-name']"));
-
-		//h4.product-name-----css
-		//h4[@class='product-name']----xpath 
-
+		
 		int j=0;
 		for(int i=0; i<=Products.size(); i++)
 		{
@@ -47,6 +61,9 @@ public class FindingThroughGeneric {
 
 
 
+		
+		
+		
 	}
 
 }
